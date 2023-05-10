07bd12dfa7202b81ac6145397104676ea8acdef0
chojangg <s2115@e-mirim.hs.kr>
2023-05-10 12:59:54 +0900

    클래스명이 겹치는 경우

--- ClassBasic/Program.cs
+++ ClassBasic/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Data;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
+    class Math
+    {
+        public static int abs(int i) { return 0; }
+    }
     internal class Program234
     {
         static void Main(string[] args)
         {
+            Math.abs(-3);
 
             OtherClass oc = new OtherClass();
 
