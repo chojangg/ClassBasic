39bbb86b19c2884902b7d05146631b5f54fd92c8
chojangg <s2115@e-mirim.hs.kr>
2023-05-10 12:57:40 +0900

    초기 커밋.

---
+++ ClassBasic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program234
+    {
+        static void Main(string[] args)
+        {
+
+            OtherClass oc = new OtherClass();
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+        class ThridClass
+        {
+
+        }
+    }
+
+    class SecondClass
+    {
+
+    }
+}
